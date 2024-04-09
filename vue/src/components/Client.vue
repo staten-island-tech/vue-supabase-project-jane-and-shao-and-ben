ebf0dcb84ccbc3d2974ea84fb3c86e8dc7452407
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2024-04-09 12:24:04 -0400

    getting data

--- vue/src/components/Client.vue
+++ vue/src/components/Client.vue
@@ -10,6 +10,8 @@ const supabase = createClient(
   'https://svdpqaqmcotuflvhbqim.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2ZHBxYXFtY290dWZsdmhicWltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIyNDg5MTEsImV4cCI6MjAyNzgyNDkxMX0.JkHVkjkHgiBuISSKlZxg9ihw4piXExdyvhTwltISRA8'
 )
+const { data, error } = await supabase.from('users').select()
+const { error } = await supabase.from('users').insert({ id: 1, name: 'Denmark' })
 </script>
 
 <style lang="scss" scoped></style>
