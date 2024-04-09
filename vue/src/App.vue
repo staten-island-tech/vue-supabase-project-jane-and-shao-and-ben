e21eca7fdf6c47a323b6df6b9c995a40c069ee84
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-04-09 12:09:27 -0400

    kisses

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './supabaseClient'
 
-<template></template>
+const accounts = ref([])
 
-<style scoped></style>
+async function getAccount() {
+  const { data } = await supabase.from('account_infomation').select()
+  accounts.value = data
+}
+
+onMounted(() => {
+  getAccount()
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="account in accounts" :key="account.id">{{ account.balance }}</li>
+  </ul>
+</template>
