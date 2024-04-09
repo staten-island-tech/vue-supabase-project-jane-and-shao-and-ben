40b7fdf6ecffe264ea5ef62419e25797d5798ace
Ben Zanelli <97360492+BenZanelli@users.noreply.github.com>
2024-04-09 12:00:04 -0400

    this doesnt work

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,23 @@
+<<<<<<< Updated upstream
 <script setup></script>
 
 <template></template>
+=======
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://svdpqaqmcotuflvhbqim.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+let { data: account_information, error } = await supabase
+  .from('account_information')
+  .select('*')
+console.log(account_information)
+</script>
+
+<template>
+
+
+</template>
+>>>>>>> Stashed changes
 
 <style scoped></style>
--- vue/src/stores/counter.js
+++ vue/src/stores/counter.js
@@ -1,6 +1,7 @@
-import { ref, computed } from 'vue'
+/* import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+<<<<<<< Updated upstream
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
@@ -10,3 +11,6 @@ export const useCounterStore = defineStore('counter', () => {
 
   return { count, doubleCount, increment }
 })
+=======
+ */
+>>>>>>> Stashed changes
