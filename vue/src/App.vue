5b494a9a7cff4fe1c57af91348c42d8aea3393b1
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-04-11 13:07:02 -0400

    66

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-<template>
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './supabaseClient'
-
-const accounts = ref([])
-
-
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://svdpqaqmcotuflvhbqim.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-let { data: account_information, error } = await supabase
-  .from('account_information')
-=======
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
@@ -22,7 +6,6 @@ async function test() {
   
 let { data: accountinformation, error } = await supabase
   .from('accountinformation')
->>>>>>> dev
   .select('*')
           
           
@@ -34,15 +17,7 @@ let { data: accountinformation, error } = await supabase
 test()
 </script>
 
-<<<<<<< HEAD
-
-
-
-</template>
-
-=======
 <template>
   <ul>
   </ul>
 </template>
->>>>>>> dev
