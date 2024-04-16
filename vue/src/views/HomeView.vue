65cfede5f8a12c125426107281318e21971fca19
Ben Zanelli <97360492+BenZanelli@users.noreply.github.com>
2024-04-16 12:58:13 -0400

    ats

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+async function fetch() {
+let { data: accountinformation, error } = await supabase
+  .from('accountinformation')
+  .select('')
+  console.log(accountinformation) 
+  if (error) {
+  console.error(error)
+}}
+fetch()
+const emails = ref("")
+const pass = ref("")
+
+async function sign() {
+  const { data, error } = await supabase.auth.signUp({
+  email: emails.value,
+    password: pass.value,
+  })
+console.log(emails.value, pass.value)
+}
+
+
+
+
+</script>
+
+<template>
+  <form action="" @submit.prevent="sign">
+    <label for="">Enter Email</label>
+    <input type="text" v-model="emails">
+    <label for="">Enter 6+ digit passsword</label>
+    <input type="text" name="" id="" v-model="pass">
+    <input type="submit" name="" id="">
+  </form>
+</template>
