dc1a70535a0293c0d4be549434662756d5779d6f
Ben Zanelli <97360492+BenZanelli@users.noreply.github.com>
2024-04-16 13:23:00 -0400

    ake this work

---
+++ vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="wrapper flex flex-row justify-center m-auto w-full">
+      <nav class="flex justify-center m-auto">
+        <button class="mr-10 bg-slate-300 border-2 border-black hover:bg-slate-600 hover:text-gray-50">
+          <RouterLink to="/" class=" ">Home</RouterLink>
+        </button>
+
+
+        <button class="bg-slate-300 border-2 border-black hover:bg-slate-600 hover:text-gray-50">
+          <RouterLink to="/about" class="]"> Graph 1</RouterLink>
+        </button>
+       
+      </nav>
+    </div>
+  </div>
+  <RouterView/>
+</template>
+
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>
+
+
+<style lang="scss" scoped>
+
+
+</style>
