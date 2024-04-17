bc4bf9d009cc1cb7cd5123aa474da9502a1f318e
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-04-17 13:03:31 -0400

    13123

--- vue/src/components/SlotsLogic.vue
+++ vue/src/components/SlotsLogic.vue
@@ -5,9 +5,53 @@
 </template>
 
 <script>
-    
+    /* export default {
+        props: {
+            list: {
+                type: Array,
+                default: () =>
+                [
+
+                ],
+            },
+        },
+
+    }, */
+ data: [
+        {
+          name: "Lemon",
+          value: 5,
+          image:
+            "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Lemon-512.png"
+        },
+        {
+          name: "Melon",
+          value: 15,
+          image:
+            "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Watermelon-512.png"
+        },
+        {
+          name: "Grapes",
+          value: 25,
+          image:
+            "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Grapes-512.png"
+        },
+        {
+          name: "Cherry",
+          value: 50,
+          image:
+            "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Cherry-512.png"
+        },
+        {
+          name: "Bar",
+          value: 100,
+          image:
+            "https://cdn4.iconfinder.com/data/icons/casino-games/512/bar-512.png"
+        }
+      ],
+
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
