f5a3c3cbaab57848c333aeaf92553545666189e1
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-04-18 13:15:37 -0400

    312

---
+++ vue-project/src/components/SlotMachine.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="slot-machine">
+    <button @click="spin">Spin</button>
+    <img :src="data[slot1].image">
+    <img :src="data[slot2].image">
+    <img :src="data[slot3].image">
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  data() {
+    return {
+      slot1: 0,
+      slot2: 0,
+      slot3: 0,
+      data: [
+        {
+          name: "Lemon",
+          value: 5,
+          image: "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Lemon-512.png"
+        },
+        {
+          name: "Melon",
+          value: 10,
+          image: "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Watermelon-512.png"
+        },
+        {
+          name: "Grapes",
+          value: 15,
+          image: "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Grapes-512.png"
+        },
+        {
+          name: "Cherry",
+          value: 25,
+          image: "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Cherry-512.png"
+        },
+        {
+          name: "Bar",
+          value: 50,
+          image: "https://cdn4.iconfinder.com/data/icons/casino-games/512/bar-512.png"
+        }
+      ]
+    }
+  },
+  methods: {
+    spin() {
+      this.slot1 = Math.floor(Math.random() * this.data.length);
+      this.slot2 = Math.floor(Math.random() * this.data.length);
+      this.slot3 = Math.floor(Math.random() * this.data.length);
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
