e846992420be4e7a222a84b672775f3a588d9516
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-04-18 13:02:41 -0400

    31

--- vue/src/components/SlotsLogic.vue
+++ vue/src/components/SlotsLogic.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>
-      
+    <div class="slot-machine">
+      <button > </button>
+      <img src={{ data[slot1].image }}>
+      <img src={{ data[slot2].image }}>
+      <img src={{ data[slot3].image }}>
+
     </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
     /* export default {
         props: {
             list: {
@@ -17,6 +22,10 @@
         },
 
     }, */
+let slot1 = 0
+let slot2 = 0
+let slot3 = 0
+
  data: [
         {
           name: "Lemon",
@@ -26,25 +35,25 @@
         },
         {
           name: "Melon",
-          value: 15,
+          value: 10,
           image:
             "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Watermelon-512.png"
         },
         {
           name: "Grapes",
-          value: 25,
+          value: 15,
           image:
             "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Grapes-512.png"
         },
         {
           name: "Cherry",
-          value: 50,
+          value: 25,
           image:
             "https://cdn4.iconfinder.com/data/icons/slot-machines/512/Cherry-512.png"
         },
         {
           name: "Bar",
-          value: 100,
+          value: 50,
           image:
             "https://cdn4.iconfinder.com/data/icons/casino-games/512/bar-512.png"
         }
--- vue/src/views/SlotsView.vue
+++ vue/src/views/SlotsView.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-
+        <SlotsLogic/>
     </div>
 </template>
 
 <script>
-   
+   import SlotsLogic from './components/SlotsLogic.vue'
+import { ref, onMounted } from 'vue'
 </script>
 
 <style scoped>
