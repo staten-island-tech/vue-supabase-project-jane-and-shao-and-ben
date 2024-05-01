c2d94fd5414171c0b0d9bdbaac0eedbde36409b5
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-05-01 13:02:24 -0400

    234

--- vue-project/src/components/SlotMachine.vue
+++ vue-project/src/components/SlotMachine.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="slot-machine">
-    <button @click="spin">Spin</button>
+    <button class = "buttn" @click="spin">Spin</button>
      <h2>{{ name }} a</h2>
      <div class='reel'>
     <img :src="data2[slot1].image">
@@ -16,7 +16,7 @@ import { ref } from 'vue'
 export default {
   data() {
     return {
-      slot1: 2,
+      slot1: 3,
       slot2: 2,
       slot3: 3,
       data2: [
@@ -50,24 +50,56 @@ export default {
   },
   methods: {
     spin() {
-      this.slot1 = Math.floor(Math.random() * this.data.length);
-      this.slot2 = Math.floor(Math.random() * this.data.length);
-      this.slot3 = Math.floor(Math.random() * this.data.length);
+      this.slot1 = Math.floor(Math.random() * this.data2.length);
+      this.slot2 = Math.floor(Math.random() * this.data2.length);
+      this.slot3 = Math.floor(Math.random() * this.data2.length);
     }
   }
 }
 </script>
 
 <style scoped>
+
 .reel {
   display: flex;
   flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    margin: 10px;
-    padding: 15px;
-    width: 200px;
-    height: 150px;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  padding: 15px;
+  width: 200px;
+  height: 150px;
+}
 
+
+.slot-machine {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+}
+
+
+.buttn {
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+
+
+.buttn:hover {
+  background-color: #45a049;
+}
+
+
+h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
 }
 </style>
+
