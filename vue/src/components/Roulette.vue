bbdbd70001fd55369f42d93e5acf729c1f397054
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2024-05-01 13:07:13 -0400

    roulette number generator

---
+++ vue/src/components/Roulette.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    function value(){
+    let randomValue = array[Math.floor(Math.random() * array.length)];
+    return { randomValue };
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
