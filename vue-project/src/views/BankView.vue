5f9738b07951deaab85c037ab05a76a750e2509a
Ben Zanelli <97360492+BenZanelli@users.noreply.github.com>
2024-05-07 13:20:24 -0400

    make add moneyz work

--- vue-project/src/views/BankView.vue
+++ vue-project/src/views/BankView.vue
@@ -3,17 +3,26 @@
         <h1 class="flex justify-center underline font-bold text-4xl">Welcome to the Bank!</h1>
         <div class="">
             <h2 class=" justify-center items-center text-center mt-10 font-medium text-2xl underline ">Account</h2>
-            <div class="dataarea justify-center m-auto mt-10 grid grid-cols-5 grid-rows-2 w-2/3">
-                <h3 class="">Last 4 digits of Account ID</h3>
-                <h3>Balance</h3>
-                <h3>Total Bet</h3>
-                <h3>Total Winnings</h3>
-                <h3>Total Losses</h3>
-                <h4>{{ accid }}</h4>
-                <h4>{{ bal }}</h4>
-                <h4>{{ totalb }}</h4>
-                <h4>{{ totalw }}</h4>
-                <h4>{{ totall }}</h4>
+            <div class="dataarea justify-center m-auto mt-10 grid grid-cols-6 grid-rows-2 w-3/4">
+                <h3 class="flex justify-center items-center">Last 4 digits of Account ID</h3>
+                <h3 class="flex justify-center items-center">Balance</h3>
+                <h3 class="flex justify-center items-center">Total Bet</h3>
+                <h3 class="flex justify-center items-center">Total Winnings</h3>
+                <h3 class="flex justify-center items-center">Total Losses</h3>
+                <h3 class="flex justify-center items-center">Add Money</h3>
+                <h4 class="flex justify-center items-center">{{ accid }}</h4>
+                <h4 class="flex justify-center items-center">{{ bal }}</h4>
+                <h4 class="flex justify-center items-center">{{ totalb }}</h4>
+                <h4 class="flex justify-center items-center">{{ totalw }}</h4>
+                <h4 class="flex justify-center items-center">{{ totall }}</h4>
+                <div class="addmoney flex flex-row items-center	">
+                    <div class="flex flex-col mr-3">
+                        <label for="addmon">Amount</label>  
+                    <input type="number" class="border-2 border-black w-[90%]" name="addmon">
+                    </div>
+                    <button class="border-2 border-black">Add Money!</button>
+                </div>
+
             </div>
         </div>
 </div>
