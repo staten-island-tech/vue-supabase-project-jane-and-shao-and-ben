fba0db161e683ee227446f7b5dc7006c90d85934
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2024-05-07 13:13:37 -0400

    roulette board numbers

--- vue/src/components/Roulette.vue
+++ vue/src/components/Roulette.vue
@@ -8,7 +8,191 @@
     function value(){
     let randomValue = array[Math.floor(Math.random() * array.length)];
     return { randomValue };
+  } 
+
+  const numbers = [
+
+  {
+    value: 1,
+    color: "red",
+    even: false
+  },
+  {
+    value: 2,
+    color: "black",
+    even: true
+  },
+  {
+    value: 3,
+    color: "red",
+    even: false
+  },
+  {
+    value: 4,
+    color: "black",
+    even: true
+  },
+  {
+    value: 5,
+    color: "red",
+    even: false
+  },
+  {
+    value: 6,
+    color: "black",
+    even: true
+  },
+  {
+    value: 7,
+    color: "red",
+    even: false
+  },
+  {
+    value: 8,
+    color: "black",
+    even: true
+  },
+  {
+    value: 9,
+    color: "red",
+    even: false
+  },
+  {
+    value: 10,
+    color: "black",
+    even: true
+  },
+  {
+    value: 11,
+    color: "black",
+    even: false
+  },
+  {
+    value: 12,
+    color: "red",
+    even: true
+  },
+  {
+    value: 13,
+    color: "black",
+    even: false
+  },
+  {
+    value: 14,
+    color: "red",
+    even: true
+  },
+  {
+    value: 15,
+    color: "black",
+    even: false
+  },
+  {
+    value: 16,
+    color: "red",
+    even: true
+  },
+  {
+    value: 17,
+    color: "black",
+    even: false
+  },
+  {
+    value: 18,
+    color: "red",
+    even: true
+  },
+  {
+    value: 19,
+    color: "red",
+    even: false
+  },
+  {
+    value: 20,
+    color: "black",
+    even: true
+  },
+  {
+    value: 21,
+    color: "red",
+    even: false
+  },
+  {
+    value: 22,
+    color: "black",
+    even: true
+  },
+  {
+    value: 23,
+    color: "red",
+    even: false
+  },
+  {
+    value: 24,
+    color: "black",
+    even: true
+  },
+  {
+    value: 25,
+    color: "red",
+    even: false
+  },
+  {
+    value: 26,
+    color: "black",
+    even: true
+  },
+  {
+    value: 27,
+    color: "red",
+    even: false
   }
+  {
+    value: 28,
+    color: "black",
+    even: true
+  },
+  {
+    value: 29,
+    color: "black",
+    even: false
+  },
+  {
+    value: 30,
+    color: "red",
+    even: true
+  },
+  {
+    value: 31,
+    color: "black",
+    even: false
+  },
+  {
+    value: 32,
+    color: "red",
+    even: true
+  },
+  {
+    value: 33,
+    color: "black",
+    even: false
+  },
+  {
+    value: 34,
+    color: "red",
+    even: true
+  },
+  {
+    value: 35,
+    color: "black",
+    even: false
+  },
+  {
+    value: 36,
+    color: "red",
+    even: true
+  },
+  ]
 </script>
 
 <style lang="scss" scoped>
