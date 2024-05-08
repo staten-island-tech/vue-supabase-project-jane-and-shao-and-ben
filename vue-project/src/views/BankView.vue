5621d893603018f4e5c7f5e13be999920b09de88
Ben Zanelli <97360492+BenZanelli@users.noreply.github.com>
2024-05-08 13:16:05 -0400

    Update BankView.vue

--- vue-project/src/views/BankView.vue
+++ vue-project/src/views/BankView.vue
@@ -1,30 +1,85 @@
 <template>
     <div>
-        <h1 class="flex justify-center underline font-bold text-4xl">Welcome to the Bank!</h1>
-        <div class="">
-            <h2 class=" justify-center items-center text-center mt-10 font-medium text-2xl underline ">Account</h2>
-            <div class="dataarea justify-center m-auto mt-10 grid grid-cols-6 grid-rows-2 w-3/4">
-                <h3 class="flex justify-center items-center">Last 4 digits of Account ID</h3>
-                <h3 class="flex justify-center items-center">Balance</h3>
-                <h3 class="flex justify-center items-center">Total Bet</h3>
-                <h3 class="flex justify-center items-center">Total Winnings</h3>
-                <h3 class="flex justify-center items-center">Total Losses</h3>
-                <h3 class="flex justify-center items-center">Add Money</h3>
-                <h4 class="flex justify-center items-center">{{ accid }}</h4>
-                <h4 class="flex justify-center items-center">{{ bal }}</h4>
-                <h4 class="flex justify-center items-center">{{ totalb }}</h4>
-                <h4 class="flex justify-center items-center">{{ totalw }}</h4>
-                <h4 class="flex justify-center items-center">{{ totall }}</h4>
-                <div class="addmoney flex flex-row items-center	">
-                    <div class="flex flex-col mr-3">
-                        <label for="addmon">Amount</label>  
-                    <input type="number" class="border-2 border-black w-[90%]" name="addmon" v-model="money">
-                    </div>
-                    <button class="border-2 border-black" @click="addmoney">Add Money!</button>
-                </div>
-
-            </div>
+        <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Bank</h1>
+      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Check Balance, Winnigs, Losses, Bets, and add money!</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Account Id (Four Digits)</h2>
+          <p class="leading-relaxed text-base">{{ accid }}</p>
         </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <circle cx="6" cy="6" r="3"></circle>
+              <circle cx="6" cy="18" r="3"></circle>
+              <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Account Balance</h2>
+          <p class="leading-relaxed text-base">${{bal}}</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Total Bets</h2>
+          <p class="leading-relaxed text-base">${{ totalb }}</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Total Winnings</h2>
+          <p class="leading-relaxed text-base">${{ totalw }}</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Total Losses</h2>
+          <p class="leading-relaxed text-base">${{ totall }}</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Add Money</h2>
+          <input class="leading-relaxed text-base border-2 border-black rounded-lg text-center" type="number" v-model="money"></input>
+        </div>
+      </div>
+    </div>
+    <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" @click="addmoney">Button</button>
+  </div>
+</section>
 </div>
 </template>
 
