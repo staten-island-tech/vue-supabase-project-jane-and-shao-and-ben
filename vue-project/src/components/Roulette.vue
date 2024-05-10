1ae0d3c5d115be204553eeebc7bc587f3cabdb1e
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2024-05-10 13:12:26 -0400

    Update Roulette.vue

--- vue-project/src/components/Roulette.vue
+++ vue-project/src/components/Roulette.vue
@@ -197,6 +197,9 @@ const numbers = [
 
 //if randomvalue=betnumbers.value, add value to bank and display
 //else, subtract bet number value from bank
+//if selected red/black, add red/black number selected value to bank
+//if selected odd/even, add odd/even number selected value to bank
+//if selected single/multi digit number, add single/multi digit number to bank
 </script>
 
 <style lang="scss" scoped></style>
